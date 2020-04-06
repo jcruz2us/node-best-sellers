46a009546db0478010034d9814f7b34119420c63
Jorge Cruz <jcruz2us@yahoo.com>
2020-04-06 12:14:03 -0700

    parse out title, description, and author

--- jobs/load_best_sellers.js
+++ jobs/load_best_sellers.js
@@ -7,6 +7,18 @@ async function run() {
   // https://www.nytimes.com/books/best-sellers/
   const bestSellersResponse = await axios.get("https://www.nytimes.com/books/best-sellers/");
   
+  // parse html using jsdom into a list of books w/ author details
+  const dom = new jsdom.JSDOM(bestSellersResponse.data);
+  const doc = dom.window.document;
+  const bookElements = Array.from(doc.querySelectorAll(`[itemtype="https://schema.org/Book"]`));  
+  
+  // iterate over list of books
+  for (let bookElement of bookElements) {
+    const author = bookElement.querySelector(`[itemprop="author"]`).textContent.replace("by", "").trim();
+    const title = bookElement.querySelector(`[itemprop="name"]`).textContent;
+    const description = bookElement.querySelector(`[itemprop="description"]`).textContent;
+    console.log({author, title, description});
+  }
 }
 
 function onJobSuccess() {
