7c4001625ce073cd8e13897bb7c0a3c55beb2dcb
Jorge Cruz <jcruz2us@yahoo.com>
2020-04-06 14:09:06 -0700

    tweak db settings

--- config/library_database.js
+++ config/library_database.js
@@ -1,8 +1,10 @@
 module.exports = {
     "development": {
         "url": process.env.DB_CONNECTION_STRING || "postgres://postgres:password@0.0.0.0/postgres",
+        "dialect": "postgres"
     },
     "production": {
         "url": process.env.DB_CONNECTION_STRING,
+        "dialect": "postgres"
     }
 }
