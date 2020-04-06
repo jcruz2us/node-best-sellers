2ed5866b9ffadc3b4c57e4b3b6cf43fff8ba048f
Jorge Cruz <jcruz2us@yahoo.com>
2020-04-06 12:02:16 -0700

    simple web server w/ code reloading

---
+++ api/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import bodyParser from 'body-parser';
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
